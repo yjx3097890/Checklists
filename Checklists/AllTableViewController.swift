e2c79da3f2dcd34177cca966a87c4340407376c9
yanjixian <315644588@qq.com>
2021-06-01 19:33:10 +0800

    merge

--- Checklists/AllTableViewController.swift
+++ Checklists/AllTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class AllTableViewController: UITableViewController {
     
-    var alllist = Array<[ChecklistItem]>()
+    var alllist = Array<Checklist>()
     let cellIdentifier = "ChecklistCell"
     
 
---
+++ Checklists/Checklist.swift
@@ -0,0 +1,15 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by yan jixian on 2021/5/31.
+//
+
+import UIKit
+
+class Checklist: NSObject, Codable {
+
+    var name: String = ""
+    var items: Array<ChecklistItem> = []
+    
+}
